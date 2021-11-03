909d2c0726c2db51d0b3d0ac7b8f69afaf430ba7
mashiike <ikeda-masashi@kayac.com>
2021-11-03 20:50:41 +0900

    utils

---
+++ utils.go
@@ -0,0 +1,32 @@
+package sffle
+
+import (
+	"strings"
+
+	"github.com/Cside/jsondiff"
+	"github.com/fatih/color"
+)
+
+func jsonDiffString(j1, j2 string) string {
+	diff := jsondiff.Diff([]byte(j1), []byte(j2))
+	var builder strings.Builder
+	c := color.New(color.Reset)
+	if diff == "" {
+		c.Fprint(&builder, j1, "\n")
+		return builder.String()
+	}
+	diffLines := strings.Split(diff, "\n")
+	for _, str := range diffLines {
+		trimStr := strings.TrimSpace(str)
+		if strings.HasPrefix(trimStr, "+") {
+			builder.WriteString(color.GreenString(str) + "\n")
+			continue
+		}
+		if strings.HasPrefix(trimStr, "-") {
+			builder.WriteString(color.RedString(str) + "\n")
+			continue
+		}
+		c.Fprint(&builder, str, "\n")
+	}
+	return builder.String()
+}
