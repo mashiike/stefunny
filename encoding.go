ee90038a30e2c23920afbf011c379b56a18d52c0
mashiike <ikeda-masashi@kayac.com>
2024-02-03 02:28:37 +0900

    Add snake case conversion for keys in YAML and JSON

--- encoding.go
+++ encoding.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"strings"
 
 	"github.com/Cside/jsondiff"
 	"github.com/fatih/color"
 	"github.com/google/go-jsonnet/formatter"
+	"github.com/serenize/snaker"
 	"gopkg.in/yaml.v3"
 )
 
@@ -152,6 +154,12 @@ func deleteNilFromMap(v map[string]interface{}) map[string]interface{} {
 			delete(v, key)
 			continue
 		}
+		if str, ok := value.(string); ok {
+			if str == "" {
+				delete(v, key)
+			}
+			continue
+		}
 		if m, ok := value.(map[string]interface{}); ok {
 			v[key] = deleteNilFromMap(m)
 			continue
@@ -174,4 +182,140 @@ func deleteNilFromMap(v map[string]interface{}) map[string]interface{} {
 		v[key] = replaceSlice
 	}
 	return v
+}
+
+// KeysToSnakeCase converts the keys of the given object to snake case.
+// The given object is expected struct, json struct key is CamelCase.
+type KeysToSnakeCase[T any] struct {
+	Value T
+}
+
+func NewKeysToSnakeCase[T any](v T) KeysToSnakeCase[T] {
+	return KeysToSnakeCase[T]{Value: v}
+}
+
+func SnakeToCamel(s string) string {
+	str := snaker.SnakeToCamel(s)
+	str = strings.Replace(str, "Cloudwatch", "CloudWatch", -1)
+	return str
+}
+
+func CamelToSnake(s string) string {
+	str := snaker.CamelToSnake(s)
+	str = strings.Replace(str, "cloud_watch", "cloudwatch", -1)
+	return str
+}
+
+func (k *KeysToSnakeCase[T]) UnmarshalYAML(value *yaml.Node) error {
+	if value.Kind != yaml.MappingNode {
+		return fmt.Errorf("KeysToSnakeCase[T] must be mapping node")
+	}
+	var data map[string]any
+	if err := value.Decode(&data); err != nil {
+		return err
+	}
+	if data == nil {
+		data = map[string]any{}
+	}
+	if err := walkMap(data, SnakeToCamel); err != nil {
+		return fmt.Errorf("failed to moddify mapping node: %w", err)
+	}
+	bs, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(bs, &k.Value); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (k *KeysToSnakeCase[T]) UnmarshalJSON(bs []byte) error {
+	var data map[string]any
+	if err := json.Unmarshal(bs, &data); err != nil {
+		return err
+	}
+	if err := walkMap(data, SnakeToCamel); err != nil {
+		return err
+	}
+	bs, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(bs, &k.Value); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (k KeysToSnakeCase[T]) MarshalYAML() (interface{}, error) {
+	bs, err := buildJSON(k.Value)
+	if err != nil {
+		return nil, err
+	}
+	var data map[string]any
+	if err := json.Unmarshal(bs, &data); err != nil {
+		return nil, err
+	}
+	if err := walkMap(data, CamelToSnake); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (k KeysToSnakeCase[T]) MarshalJSON() ([]byte, error) {
+	bs, err := buildJSON(k.Value)
+	if err != nil {
+		return nil, err
+	}
+	var data map[string]any
+	if err := json.Unmarshal(bs, &data); err != nil {
+		return nil, err
+	}
+	if err := walkMap(data, CamelToSnake); err != nil {
+		return nil, err
+	}
+	return json.Marshal(data)
+}
+
+func walkMap(data map[string]any, keyModifier func(string) string) error {
+	for k, v := range data {
+		delete(data, k)
+		newKey := keyModifier(k)
+		if v == nil {
+			continue
+		}
+		data[newKey] = v
+		switch v := v.(type) {
+		case map[string]any:
+			if err := walkMap(v, keyModifier); err != nil {
+				return err
+			}
+		case []any:
+			if err := walkSlilce(v, keyModifier); err != nil {
+				return err
+			}
+		default:
+			continue
+		}
+	}
+	return nil
+}
+
+func walkSlilce(data []any, keyModifier func(string) string) error {
+	for i := 0; i < len(data); i++ {
+		switch v := data[i].(type) {
+		case map[string]any:
+			if err := walkMap(v, keyModifier); err != nil {
+				return err
+			}
+		case []any:
+			if err := walkSlilce(v, keyModifier); err != nil {
+				return err
+			}
+		default:
+			continue
+		}
+	}
+	return nil
 }
